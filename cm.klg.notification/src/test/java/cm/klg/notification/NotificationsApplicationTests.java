37bb9699bfafb856876d2f10742cd2cb286f85a9
JAURES KAMENI NGUEMATCHA <kameni.nguematcha@lao-sarl.cm>
2026-08-11 12:20:29 +0100

    chlore(DEVELOP):initial commit

---
+++ cm.klg.notification/src/test/java/cm/klg/notification/NotificationsApplicationTests.java
@@ -0,0 +1,11 @@
+package cm.klg.notification;
+
+import org.junit.jupiter.api.Test;
+
+class NotificationsApplicationTests {
+
+  @Test
+  void test() {
+    System.out.println("Hello World");
+  }
+}
